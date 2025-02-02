970932b6560e73f481dfe531bb80d702d64f7411
sabaelkanishvili <dudenamedsaba@gmail.com>
2025-02-02 15:08:55 +0400

    first commit

---
+++ day 01/home work/day1.py
@@ -0,0 +1,32 @@
+name= "saba"
+#name aris cvladi
+# = aris cvkadistvis  mnishvnelobis mimnichebeli simbolo 
+#saba aris cvladistvis mnishvneloba 
+
+surname= "elkanishvili"
+
+print(name) 
+#print funqcias gadaecema ekranze gamosatani obieqti 
+
+
+age= 14 # es aris integeri mteli ricxvi 
+height= 170 #es aris float tipis cvladi anu atwiladi 
+#boolean bool tipis cvladi 
+
+knows_programing = True #true an false 
+is_ugly= False 
+
+isUgly = False 
+
+
+#print(name + " " + surname) 
+
+#print(name + age ) 
+
+
+
+#print(type(age)) 
+#print(type(name)) 
+#print(type(height)) 
+#print(type(knows_programing)) 
+print(name + " " + str(age))
---
+++ day 01/home work/lesoon1.py
@@ -0,0 +1,9 @@
+name = "saba"
+surname = "elkanishvili" 
+age = 14 
+height = 170  
+city = "tbilisi" 
+country = "georgia" 
+print(name) 
+print(name + " " + str(age)) 
+print("l live in " + city +", " +country + " and l am " + str(age) + " years old.")
