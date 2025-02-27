6d1051287d05b171edae3b9969ec823722e195f1
sabaelkanishvili <dudenamedsaba@gmail.com>
2025-02-27 18:38:02 +0400

    commit

---
+++ day 06/classwork/cw.py
@@ -0,0 +1,5 @@
+my_balance = 9  #ვწერთ ცვლად ბალანსს რომელიც არის 9  
+my_balance = my_balance + 2   #შემდეგ ვამატებთ 2  
+my_balance += 8  #შემდეგ კიდებ 8 მაგრამ სხვა გზით 
+my_balance += 3  #და ბოლოს 3     
+print(my_balance)
---
+++ day 06/homework/hw.py
@@ -0,0 +1,5 @@
+client_name = "john"   
+client_last_name= "wick" 
+age = 52 
+city = "new york" 
+print(client_name + " " + client_last_name + " " + str(age) + " " + city)
