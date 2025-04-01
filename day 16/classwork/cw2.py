391d2a4f3a55e75e78978dd4285cd07d0d9b2389
sabaelkanishvili <dudenamedsaba@gmail.com>
2025-04-01 23:31:09 +0400

    commit

---
+++ day 16/classwork/cw2.py
@@ -0,0 +1,5 @@
+number1 = int(input("enter first number ")) 
+number2 = int(input("enter second number ")) 
+
+for num in range(number1,number2 + 2): 
+    print(num)
---
+++ day 16/classwork/cw3.py
@@ -0,0 +1,9 @@
+password = "saba2010"  
+user_entry = "" 
+
+
+while user_entry != password:  
+    user_entry =input("enter password")  
+
+
+print("logged in")
